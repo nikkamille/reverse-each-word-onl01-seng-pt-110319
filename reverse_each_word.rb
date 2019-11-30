ee0a6ac2e30e7a9eeb8d70c7605ca87c606d6a09
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-30 04:39:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   # sentence1.split.collect {|word| word.reverse}.join(" ")
   sentence1.split.collect do |word|
-    return (word.reverse).join(" ")
+    return word.join(" ")
   end
 end
   
