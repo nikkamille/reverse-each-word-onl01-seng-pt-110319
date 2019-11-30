54006845882c7c38d70d87794e3418064ab8cc33
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-30 04:31:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(sentence1)
-  sentence1 = []
-  sentence1 << "Hello there, and how are you?".split
-  sentence1.each do |word|
-    puts word.reverse
-  end
+  sentence1.split
 end
   
